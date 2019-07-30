fd39708a934519c992e768dc7df04b5de0ef9b0d
srinigandhe <srinivas.gandhe@accenture.com>
2019-07-30 20:09:35 +0200

    destructuring nested objects and type annotations

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,36 @@ const today = {
     weather: 'Sunny'
 }
 
-const logWeather = (forecast: {date: Date, weather: string}): string => {
+const logWeather = (forecast: { date: Date, weather: string }): string => {
     return `On ${forecast.date} the weather is/was ${forecast.weather}`
 };
 
-const destructuredLogWeather = ({date, weather}:{date: Date, weather: string}): void => {
+const destructuredLogWeather = ({date, weather}: { date: Date, weather: string }): void => {
     console.log(`${date} :: ${weather}`)
 };
 
 
 console.log(logWeather(today));
 console.log(destructuredLogWeather(today));
+
+const profile = {
+    myName: 'Alex',
+    age: 20,
+    coords: {
+        lat: 0,
+        lng: 15
+    },
+    setAge(age: number): void {
+        this.age = age;
+    }
+};
+
+profile.setAge(30);
+const {age, myName}: { age: number, myName: string } = profile;
+
+console.log(age, myName);
+
+
+const {coords: {lat, lng}}: { coords: { lat: number, lng: number } } = profile;
+console.log(lat, lng);
+
